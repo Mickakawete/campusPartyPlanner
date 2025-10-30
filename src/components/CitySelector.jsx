17e80bf9e1864a15171cba6e0d31165e8c4984e2
Mickael <monemail@gmail.com>
2025-10-30 16:18:23 +0100

    FIX: correctif merge

--- src/components/CitySelector.jsx
+++ src/components/CitySelector.jsx
@@ -3,17 +3,18 @@ import EventContext from "../context/EventContext";
 import "../styles/components/city-selector.css";
 
 export const CitySelector = () => {
-  const { cities, selectedCity, setCity } = useContext(EventContext);
+    const { cities, selectedCity, setCity } = useContext(EventContext);
 
-  return (
-    <>
-      <section className="section-city-selector">
-        <select className="section-city-selector__dropdown" value={selectedCity || ""} onChange={(e) => setCity(e.target.value)}>
-          {cities.map((city) => (
-            <option key={city} className="section-city-selector__dropdown__option" value={city}>{city}</option>
-          ))}
-        </select>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className="section-city-selector">
+                <select className="section-city-selector__dropdown" value={selectedCity || "#"} onChange={(e) => setCity(e.target.value || null)}>
+                    <option value="">Tous les événements</option>
+                    {cities.map((city) => (
+                        <option key={city} className="section-city-selector__dropdown__option" value={city}>{city}</option>
+                    ))}
+                </select>
+            </section>
+        </>
+    );
 };
